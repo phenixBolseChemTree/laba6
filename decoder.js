8831302f1d5e8ceda4e0863fe449a126a3f1531b
ivan <oldrum75@gmail.com>
2022-09-17 01:28:13 +0600

    +1

--- decoder.js
+++ decoder.js
@@ -1,13 +1,3 @@
-const collect = [
-  [1, 'a', 'b', 'c', 'd', 'e'],//0
-  [2, 'f', 'g', 'h', 'i', 'k'],//1
-  [3, 'l', 'm', 'n', 'o', 'p'],//2
-  [4, 'q', 'r', 's', 't', 'u'],//3
-  [5, 'v', 'w', 'x', 'y', 'z'],//4
-];
-
-//hikka
-
 const deInicializator = (str) => {
   for (let i = 0; i < 5; i += 1) {
     for (let v = 1; v < 6; v += 1) {
@@ -25,8 +15,3 @@ const deCoder = (str) => {
   })
   return result.join(' ');
 }
-
-
-console.log(deInicializator('h'));
-console.log(deInicializator('h'));
-console.log(deCoder('deuhikka')); 
